a59df8051d148a2629846cba4af1015052083cd4
Mehdi Ben Amor <mehdi.benamor@uni-passau.de>
2022-11-03 16:07:42 +0100

    rollback on unwanted change

--- experiments/bert_position_bias.py
+++ experiments/bert_position_bias.py
@@ -195,19 +195,19 @@ def main():
 
         task_trainer.log_pos_losses()
 
-        # if args.duplicate:
-        #     for k in range(1, 11):
-        #         test_dataset = dataset.dataset["test_"].map(processor.tokenize_and_align_labels,
-        #                                                     fn_kwargs={"duplicate": args.duplicate, "k": k},
-        #                                                     load_from_cache_file=False, batched=True)
-        #
-        #         task_trainer.test(test_dataset=test_dataset, metric_key_prefix=f"test_k={k}", k=k)
-        # else:
-        #     test_dataset = dataset.dataset["test_"].map(processor.tokenize_and_align_labels,
-        #                                                 fn_kwargs={"duplicate": True, "k": 1},
-        #                                                 load_from_cache_file=False,
-        #                                                 batched=True)
-        #     task_trainer.test(test_dataset=test_dataset, metric_key_prefix=f"test_k=10", k=1)
+        if args.duplicate:
+            for k in range(1, 11):
+                test_dataset = dataset.dataset["test_"].map(processor.tokenize_and_align_labels,
+                                                            fn_kwargs={"duplicate": args.duplicate, "k": k},
+                                                            load_from_cache_file=False, batched=True)
+
+                task_trainer.test(test_dataset=test_dataset, metric_key_prefix=f"test_k={k}", k=k)
+        else:
+            test_dataset = dataset.dataset["test_"].map(processor.tokenize_and_align_labels,
+                                                        fn_kwargs={"duplicate": True, "k": 1},
+                                                        load_from_cache_file=False,
+                                                        batched=True)
+            task_trainer.test(test_dataset=test_dataset, metric_key_prefix=f"test_k=10", k=1)
 
         wandb.finish()
         task_trainer = None
